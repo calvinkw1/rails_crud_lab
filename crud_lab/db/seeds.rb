31aee8ef608c0c3240c5990c0ee8f84f1b4ba146
Calvin Wang <calvinkw1@gmail.com>
2015-02-20 11:14:50 -0800

    completed hw

---
+++ crud_lab/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Puppy.create ([{ name: 'Mocha', age: 3, breed: 'Cockapoo' }])
+Puppy.create ([{ name: 'Vin', age: 4, breed: 'Mini Pinscher' }])
+Puppy.create ([{ name: 'Ken', age: 5, breed: 'Golden Doodle' }])
+Puppy.create ([{ name: 'Barbie', age: 8, breed: 'Yorkie' }])
